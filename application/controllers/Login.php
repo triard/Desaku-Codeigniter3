ae8af9ed898196f930a1e5ee398cf924ad863178
wsngiri <prolink714@gmail.com>
2020-03-16 06:24:21 +0700

    menambahkan login tapi kurang message lak gagal login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,10 +1,47 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
+
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->library('session');
+	}
+
 
 	public function index()
 	{
-		$this->load->view('login/index');
+		$this->form_validation->set_rules('username', 'username', 'trim|required');
+		$this->form_validation->set_rules('password', 'password', 'trim|required');
+		if ($this->form_validation->run() == FALSE) {
+			$data['title'] = "Login Admin | Desa Trate";
+			$this->load->view('login/index', $data);
+		} else {
+			$this->login();
+		}
+	}
+
+	public function login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+
+		$user = $this->db->get_where('user', ['username' => $username])->row_array();
+
+		if ($user) {
+			if ($password == $user['password']) {
+				redirect('user');
+			} else {
+				redirect('login');
+			}
+		} else {
+			echo "<script type='text/javascript'>alert('username atau password salah');</script>";
+			redirect('login');
+		}
 	}
 }
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,77 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+	<title><?php echo $title; ?></title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-	<title>Login | Desaku</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 	<style>
-		.card-header{
+		.card-header {
 			background-image: url('<?php echo base_url('assets/login.jpg'); ?>');
-			padding:15px 15px 44px; 
-			background-repeat:no-repeat;
-    		background-size:cover;
-			background-position:center;
-			
+			padding: 15px 15px 44px;
+			background-repeat: no-repeat;
+			background-size: cover;
+			background-position: center;
+
 		}
 	</style>
 </head>
+
 <body>
-<div class="d-flex justify-content-center">
-	<div class="container m-4 pb-5">
-	<div class="d-flex mt-5 justify-content-center">
-		<div class="col-sm-9">
-			<div class="card">
-  			<div class="card-header text-center text-white">
-    			<h1>Sign In</h1>
-  			</div>
-  			<div class="card-body">
-			  <form action="/action_page.php" class="needs-validation" novalidate>
-    			<div class="form-group">
-      				<label for="uname">Username:</label>
-      				<input type="text" class="form-control" id="uname" placeholder="Enter username" name="uname" required>
-      				<div class="valid-feedback">Valid.</div>
-      				<div class="invalid-feedback">Please fill out this field.</div>
-    			</div>
-    			<div class="form-group">
-      				<label for="pwd">Password:</label>
-      				<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
-      				<div class="valid-feedback">Valid.</div>
-      				<div class="invalid-feedback">Please fill out this field.</div>
-    			</div>
-    			<button type="submit" class="btn btn-primary">Login</button>
-  			   </form>
-  			</div>
-  		    <div class="card-footer text-muted text-center">
-				  <a class="btn btn-danger" href="<?php echo base_url('Welcome') ?>">Back</a>
-  			</div>
+	<div class="d-flex justify-content-center">
+		<div class="container m-4 pb-5">
+			<div class="d-flex mt-5 justify-content-center">
+				<div class="col-sm-9">
+					<div class="card">
+						<div class="card-header text-center text-white">
+							<h1>Sign In</h1>
+						</div>
+						<div class="card-body">
+							<form method="post" class="needs-validation" novalidate>
+								<div class="form-group">
+									<label for="username">Username:</label>
+									<input type="text" class="form-control" id="username" placeholder="Enter username" name="username" required>
+									<div class="valid-feedback">Valid.</div>
+									<div class="invalid-feedback">Please fill out this field.</div>
+								</div>
+								<div class="form-group">
+									<label for="password">Password:</label>
+									<input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required>
+									<div class="valid-feedback">Valid.</div>
+									<div class="invalid-feedback">Please fill out this field.</div>
+								</div>
+								<button type="submit" class="btn btn-primary">Login</button>
+							</form>
+						</div>
+						<div class="card-footer text-muted text-center">
+							<a class="btn btn-danger" href="<?php echo base_url('Welcome') ?>">Back</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-	</div>
-</div>
 </body>
 <script>
-// Disable form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Get the forms we want to add validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
+	// Disable form submissions if there are invalid fields
+	(function() {
+		'use strict';
+		window.addEventListener('load', function() {
+			// Get the forms we want to add validation styles to
+			var forms = document.getElementsByClassName('needs-validation');
+			// Loop over them and prevent submission
+			var validation = Array.prototype.filter.call(forms, function(form) {
+				form.addEventListener('submit', function(event) {
+					if (form.checkValidity() === false) {
+						event.preventDefault();
+						event.stopPropagation();
+					}
+					form.classList.add('was-validated');
+				}, false);
+			});
+		}, false);
+	})();
 </script>
+
 </html>
