6612d225b6ff2a5f2b6c27e6873956dc3085d9b0
triard <triard78@gmail.com>
2020-03-23 10:07:47 +0700

    perbaikan bug pengaduan dan penambahan fitur surat online

--- application/views/admin/pengaduan.php
+++ application/views/admin/pengaduan.php
@@ -5,29 +5,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
+    <?php $this->load->view("admin/_partials/head.php") ?>
 </head>
 
-<body>
-    <h1>Daftar Pengaduan</h1>
+<body id="page-top">
+<?php $this->load->view("admin/_partials/navbar.php") ?>
+<div id="wrapper">
+<?php $this->load->view("admin/_partials/sidebar.php") ?>
 
-    <table>
+<div id="content-wrapper">
 
-        <tr>
-            <th>Nama</th>
-            <th>Nik</th>
-            <th>Isi</th>
-            <th>Detail</th>
-        </tr>
-        <?php foreach ($pengaduan as $pd) : ?>
-            <tr>
-                <td><?= $pd["nama"] ?></td>
-                <td><?= $pd["nik"] ?></td>
-                <td><?= $pd["isi"] ?></td>
-                <td>4</td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+    <div class="container-fluid">
+    <h1>Daftar Pengaduan Penduduk</h1>
 
+        <div style="overflow-y: scroll;overflow-x: scroll; width: 100%;border: 1px black dotted;">        
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nama Pengadu</th>
+                    <th>Perihal</th>
+                    <th>Tanggal Pengaduan</th>
+                   <th>Detail</th>
+                </tr>
+            </thead>
+        <tbody>
+            <?php foreach ($pengaduan as $pd) : ?>
+                <tr>
+                    <td><?= $pd["nama"] ?></td>
+                    <td><?= $pd["perihal"] ?></td>
+                    <td><?= $pd["tanggal"] ?></td>
+                    <td> <a class="btn btn-primary" href="<?php echo base_url('admin/Pengaduan/pengaduan_detail/'. $pd['id']) ?>">detail</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+        </table>
+        </div>
+    <?php $this->load->view("admin/_partials/footer.php") ?>
+    </div>
+</div>
+
+    <?php $this->load->view("admin/_partials/scrolltop.php") ?>
+    <?php $this->load->view("admin/_partials/modal.php") ?>
+    <?php $this->load->view("admin/_partials/js.php") ?>
 </body>
-
 </html>
--- application/views/usersLogin/surat_online.php
+++ application/views/usersLogin/surat_online.php
@@ -4,12 +4,38 @@
             <div class="card-header">Data Permintaan Surat Online</div>
             <div class="card-body">
                 <a href="<?php echo base_url('UserLogin/surat_online_add_data') ?>" class="btn btn-success">Data Baru</a>
+                <h1>Daftar Permintaan Surat Anda</h1>
+                
+                <div style="overflow-y: scroll;overflow-x: scroll; width: 100%;border: 1px black dotted;">        
 
-                     </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nama</th>
+                            <th>nik</th>
+                            <th>No_telp</th>
+                            <th>category</th>
+                            <th>isi</th>
+                            <th>tanggal permohonan</th>
+                            <th>status</th>
+                        </tr>
+                    </thead>
+                <tbody>
+                <?php foreach ($surat as $st) : ?>
+                    <tr>
+                    <td><?= $st["nama"] ?></td>
+                    <td><?= $st["nik"] ?></td>
+                    <td><?= $st["no_telp"] ?></td>
+                    <td><?= $st["category"] ?></td>
+                    <td><?= $st["isi"] ?></td>
+                    <td><?= $st["tgl_permohonan"] ?></td>
+                    <td><?= $st["status"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+                </table>
                  </div>
-
             </div>
         </div>
-    </div>
-  </div>
-  <?php $this->load->view("usersLogin/template/footer.php") ?>
+</div>
+<?php $this->load->view("usersLogin/template/footer.php") ?>
